08e90b89de5c590858aede2dd45a535204aefb8f
TheRealMPC <smkscar25@hotmail.com>
2017-07-25 00:35:20 -0500

    DevMtn-Travels finished

---
+++ js/controllers/bookedCtrl.js
@@ -0,0 +1,8 @@
+angular.module('devmtnTravel').controller('bookedCtrl', function($scope, mainSrv, $stateParams) {
+    $scope.packages = mainSrv.packageInfo;
+    for (var i = 0; i < $scope.packages.length; i++) {
+        if ($scope.packages[i].id === Number($stateParams.id)) {
+            $scope.curPackage = $scope.packages[i];
+        }
+    }
+})
---
+++ js/controllers/packagesCtrl.js
@@ -0,0 +1,3 @@
+angular.module('devmtnTravel').controller('packagesCtrl', function($scope, $stateParams, mainSrv){
+     $scope.packages = mainSrv.packageInfo;
+})
